9d98ae156e4b685dd257a5b94f6fec83bfa27b87
Valerii <vgolybenko@gmail.com>
2021-06-01 20:58:22 +0300

    added files

--- index.js
+++ index.js
@@ -1,10 +1,90 @@
 "use strict"
-function templ(color){
-    const template = document.createElement("template");
-    const box = document.createElement("div");
-    box.style.backgroundColor='color';
-    document.getElementById('game').appendChild(template);
-    template.appendChild(box);
-    
-} 
-templ(red)
+const mainHolder = document.getElementById("main");
+const h1 = document.createElement("h1");
+h1.innerText = "COLOR BOX";
+mainHolder.appendChild(h1);
+const countSteps = document.createElement("span");
+countSteps.innerText = "Steps ";
+main.appendChild(countSteps);
+const gameBlock = document.createElement("div");
+gameBlock.id= "game";
+main.appendChild(gameBlock);
+const holderButton = document.createElement('div')
+holderButton.id = "holderButton";
+main.appendChild(holderButton);
+const buttonStart = document.createElement("button");
+buttonStart.innerText = "START";
+const buttonReset = document.createElement("button");
+buttonReset.innerText = "RESET";
+holderButton.append(buttonStart);
+holderButton.append(buttonReset);
+let firstColor;
+let selectedColors = [];
+let steps = 0;
+let colorArr = [
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "aqua",
+    "lime",
+    "orange",
+    "magenta",
+    "indigo",
+    "brown",
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "aqua",
+    "lime",
+    "orange",
+    "magenta",
+    "indigo",
+    "brown"
+];
+const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+shuffle(colorArr);
+const boxTemplate = (color,index) =>
+`<div style='width:100px;height:100px;background:${color}' id=${index}></div>`;
+
+gameBlock.innerHTML=colorArr.map((item,index)=>boxTemplate(item,index)).join("");
+setTimeout(
+    () => {
+gameBlock.innerHTML=colorArr.map((item,index)=>boxTemplate("black",index)).join("");
+    },4000);
+
+gameBlock.addEventListener("click", function(e){
+    addColor(colorArr[e.target.id],e.target);
+    }
+)
+
+const addColor = (color,box) => {
+    setSteps();
+    box.style.background = color;
+    if (!firstColor){
+        firstColor = color;
+        return;
+    }
+    if(compareColors(firstColor,color)){
+        selectedColors.push(color);
+    }
+    box.style.background = 'black';
+};
+
+const compareColors = (firstColor,secondColor) => {
+    clearFirstColor();
+    return firstColor === secondColor;
+}
+const clearFirstColor = () => {
+   firstColor=undefined;
+}
+const setSteps = () => {
+    steps++;
+    countSteps.innerText = 'Steps '+steps;
+}
