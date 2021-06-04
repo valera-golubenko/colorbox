078a559edb6b0d0a50e7f850e46d495df960d44d
Голубенко Валерій Володимирович <2015adm01@MISTO.KH.FSSU.GOV.UA>
2021-06-04 12:24:25 +0300

    add buttons code

--- index.js
+++ index.js
@@ -43,18 +43,27 @@ let colorArr = [
     "indigo",
     "brown"
 ];
-const shuffle = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-shuffle(colorArr);
 const boxTemplate = (color,index) =>
 `<div style='width:100px;height:100px;background:${color}' id=${index}></div>`;
 
 gameBlock.innerHTML=colorArr.map((item,index)=>boxTemplate(item,index)).join("");
-setTimeout(
+
+buttonStart.addEventListener("click", function(e){startGame()});
+buttonReset.addEventListener("click", function(e){location.reload()});
+
+const startGame = () => {
+
+    const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    shuffle(colorArr);
+
+    gameBlock.innerHTML=colorArr.map((item,index)=>boxTemplate(item,index)).join("");
+    setTimeout(
     () => {
 gameBlock.innerHTML=colorArr.map((item,index)=>boxTemplate("black",index)).join("");
     },4000);
@@ -67,23 +76,35 @@ gameBlock.addEventListener("click", function(e){
 const addColor = (color,box) => {
     setSteps();
     box.style.background = color;
+
     if (!firstColor){
         firstColor = color;
         return;
     }
     if(compareColors(firstColor,color)){
         selectedColors.push(color);
+        stopGame();
     }
-};
+}
 
 const compareColors = (firstColor,secondColor) => {
     clearFirstColor();
     return firstColor === secondColor;
 }
+
 const clearFirstColor = () => {
-   firstColor=undefined;
+    firstColor=undefined;
 }
+
 const setSteps = () => {
     steps++;
     countSteps.innerText = 'Steps '+steps;
 }
+
+const stopGame = () => {
+    if (selectedColors.length===10){
+        alert("Игра окончена!!!")
+        }
+    }
+}
+
